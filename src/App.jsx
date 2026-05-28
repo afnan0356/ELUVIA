21abda4952837b0765b45582aef38edaf6d1a89b
AFNAN <mdafnan0356@gmail.com>
2026-05-28 11:24:19 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 import HomePage from './pages/HomePage'
 import SearchPage from './pages/SearchPage'
 import ColorDetailPage from './pages/ColorDetailPage'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/search" element={<SearchPage />} />
-      <Route path="/color/:name" element={<ColorDetailPage />} />
-    </Routes>
+    <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', background: 'var(--bg-primary)' }}>
+      <Navbar />
+      <main style={{ flex: 1 }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/color/:name" element={<ColorDetailPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   )
 }
