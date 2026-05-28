df3a565c46f0b5f74ad23351e681f12f18b1dea6
AFNAN <mdafnan0356@gmail.com>
2026-05-28 16:16:50 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/global.css'
 
+// GitHub Pages SPA redirect restore
+const redirect = sessionStorage.getItem('spa_redirect')
+if (redirect) {
+  sessionStorage.removeItem('spa_redirect')
+  window.history.replaceState(null, '', '/eluvia' + redirect)
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/eluvia">
